e1ef344ff9f7f34a95e0fd483861edd99f53b529
Daniel Succar <danielsuccar@icloud.com>
2020-01-06 20:40:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
+  var dwarvesArray = []
+function dwarfRollCall() {
+
+  for (let i = 0; i < dwarves.length; i++){
+    dwarvesArray.push(`${i+1}. ${dwarves[i]}`);
+
+  }
+  return dwarvesArray.join(' ');
+
 }
+
+
 
 function summonCaptainPlanet(planeteerCalls){
-}
+
+  var result = [];
+    planeteerCalls.forEach(function(elem) {
+    	var elem = elem.toUpperCase() + "!";
+    	result.push(elem);
+    });
+    return result;
+  }
+
+
+//  return  result[i].toUpperCase()+"!";
+
 
 function longPlaneteerCalls(words) {
+	for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
 }
 
-function findTheCheese (foods) {
+
+
+function findTheCheese(foods) {
+  
+var cheeseInCommon = foods.indexOf('cheddar')
+
+if (cheeseInCommon > [-1]) {
+  return cheese[cheeseInCommon]
 }
+else {
+  return 'no cheese!'
+}
+}
+
+
+
+
+
+  // for(var i = 0; i < foods.length; i++){
+  //   for(var j =0; j < cheese.length; j++){
+  //
+  //     if(foods[i] = cheese[j]){
+  //       return cheese[j]
+
+
+
+
+
+  //  {return 'no cheese!';
+  //     }
